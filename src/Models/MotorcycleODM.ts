26d4519a84baba72cae7aa88afcc709577b5f536
Igor Arecippo <iarecippo@gmail.com>
2023-05-02 20:54:26 -0300

    fix: edit MotorcycleODM

--- src/Models/MotorcycleODM.ts
+++ src/Models/MotorcycleODM.ts
@@ -1,27 +1,18 @@
-import { Model, Schema, model, models } from 'mongoose';
+import { Schema } from 'mongoose';
+import AbstractODM from './AbstractODM';
 import IMotorcycle from '../Interfaces/IMotorcycle';
-// import HttpException from '../Middlewares/ErrorHandler';
 
-export default class MotorcycleODM {
-  private schema: Schema;
-  private model: Model<IMotorcycle>;
-
+export default class CarODM extends AbstractODM<IMotorcycle> {
   constructor() {
-    this.schema = new Schema<IMotorcycle>(
-      {
-        model: { type: String, required: true },
-        year: { type: Number, required: true },
-        color: { type: String, required: true },
-        status: { type: Boolean },
-        buyValue: { type: Number, required: true },
-        category: { type: String, required: true },
-        engineCapacity: { type: Number, required: true },
-      },
-    );
-    this.model = models.Car || model('Car', this.schema);
-  }
-
-  public async create(car: IMotorcycle): Promise<IMotorcycle> {
-    return this.model.create({ ...car });
+    const schema = new Schema<IMotorcycle>({
+      model: { type: String, required: true },
+      year: { type: Number, required: true },
+      color: { type: String, required: true },
+      status: { type: Boolean },
+      buyValue: { type: Number, required: true },
+      category: { type: String, required: true },
+      engineCapacity: { type: Number, required: true },
+    });
+    super(schema, 'Motorcycle');
   }
 }
