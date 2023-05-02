b662007950267043d42d2105c42d3e26a852854b
Igor Arecippo <iarecippo@gmail.com>
2023-05-02 20:52:04 -0300

    fix: ajustando a model CarODM para o uso da AbstractODM

--- src/Models/AbstractODM.ts
+++ src/Models/AbstractODM.ts
@@ -1,4 +1,5 @@
-import { Model, Schema, model, models } from 'mongoose';
+import { Model, Schema, UpdateQuery, isValidObjectId, model, models } from 'mongoose';
+import HttpException from '../Middlewares/ErrorHandler';
 
 export default abstract class AbstractODM<T> {
   protected schema: Schema;
@@ -7,5 +8,30 @@ export default abstract class AbstractODM<T> {
   constructor(schema: Schema, modelName: string) {
     this.schema = schema;
     this.model = models[modelName] || model(modelName, schema);
+  }
+
+  public async create(data: T): Promise<T> {
+    return this.model.create({ ...data });
+  }
+
+  public async findAll(): Promise<T[]> {
+    return this.model.find({});
+  }
+
+  public async findById(id: string) {
+    if (!isValidObjectId(id)) {
+      throw new HttpException(422, 'Invalid mongo id');
+    }
+    const result = await this.model.findOne({ _id: id }, { __v: false });
+    // console.log(result, id, 'model');
+    return result;
+  }
+
+  public async updateCar(id: string, data: T) {
+    if (!isValidObjectId(id)) {
+      throw new HttpException(422, 'Invalid mongo id');
+    }
+    await this.model.findByIdAndUpdate({ _id: id }, { ...data as UpdateQuery<T> });
+    return this.findById(id);
   }
 }
--- src/Models/CarODM.ts
+++ src/Models/CarODM.ts
@@ -1,13 +1,13 @@
-import { Model, Schema, model, models, isValidObjectId } from 'mongoose';
+import { Schema } from 'mongoose';
 import ICar from '../Interfaces/ICar';
-import HttpException from '../Middlewares/ErrorHandler';
+import AbstractODM from './AbstractODM';
+// import HttpException from '../Middlewares/ErrorHandler';
 
-export default class CarODM {
-  private schema: Schema;
-  private model: Model<ICar>;
-
+export default class CarODM extends AbstractODM<ICar> {
+  // private schema: Schema;
+  // private model: Model<ICar>;
   constructor() {
-    this.schema = new Schema<ICar>(
+    const schema = new Schema<ICar>(
       {
         model: { type: String, required: true },
         year: { type: Number, required: true },
@@ -18,31 +18,7 @@ export default class CarODM {
         seatsQty: { type: Number, required: true },
       },
     );
-    this.model = models.Car || model('Car', this.schema);
-  }
-
-  public async create(car: ICar): Promise<ICar> {
-    return this.model.create({ ...car });
-  }
-
-  public async findAll(): Promise<ICar[]> { 
-    return this.model.find({});
-  }
-
-  public async findById(id: string) {
-    if (!isValidObjectId(id)) {
-      throw new HttpException(422, 'Invalid mongo id');
-    }
-    const result = await this.model.findOne({ _id: id }, { __v: false });
-    // console.log(result, id, 'model');
-    return result;
-  }
-
-  public async updateCar(id: string, car: ICar) {
-    if (!isValidObjectId(id)) {
-      throw new HttpException(422, 'Invalid mongo id');
-    }
-    await this.model.findByIdAndUpdate({ _id: id }, { ...car });
-    return this.findById(id);
+    // this.model = models.Car || model('Car', this.schema);
+    super(schema, 'Car');
   }
 }
