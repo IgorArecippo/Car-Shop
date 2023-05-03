fa70c65ef1ca91874a87be447fe928321fe1f8b9
Igor Arecippo <iarecippo@gmail.com>
2023-05-02 21:52:54 -0300

    Update services.test.ts

--- tests/unit/Services/services.test.ts
+++ tests/unit/Services/services.test.ts
@@ -1,7 +1,11 @@
-import 'mocha';
-import { expect } from 'chai';
-import sinon from 'sinon';
-import MotoService from '../../../src/Services/MotoService'; 
-import MotorcycleODM from '../../../src/Models/MotorcycleODM';
-import IMotorcycle from '../../../src/Interfaces/IMotorcycle';
-import { mockMoto } from '../../Mocks'; 
+// import 'mocha';
+// import { expect } from 'chai';
+// import sinon from 'sinon';
+// import MotoService from '../../../src/Services/MotoService'; 
+// import MotorcycleODM from '../../../src/Models/MotorcycleODM';
+// import IMotorcycle from '../../../src/Interfaces/IMotorcycle';
+// import { mockMoto } from '../../Mocks'; 
+
+// describe('Tests Motorcycle service', function() {
+
+// })
